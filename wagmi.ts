27fe2261d04ee2f04fd960d1ea3bad2618a71810
boen <workingxw@hotmail.com>
2025-06-05 22:19:45 +0800

    fix: 修改

---
+++ wagmi.ts
@@ -0,0 +1,25 @@
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+  localhost,
+} from 'wagmi/chains';
+
+export const config = getDefaultConfig({
+  appName: 'RainbowKit App',
+  projectId: 'd623c4e74d2b5605931df007b6f40df2',
+  chains: [
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    localhost,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
+  ],
+  ssr: false,
+});
