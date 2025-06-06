316136a763be700814abbcc0dcdf9a169d9dcab6
bourne.xiao <bourne.xiao@corp.to8to.com>
2025-06-06 18:28:22 +0800

    fix: 抢红包功能完成

--- app/page.tsx
+++ app/page.tsx
@@ -10,30 +10,32 @@ import {
 } from "@/lib/hooks/useRedPacket";
 export default function Home() {
   const {
-    balance,
+    accountBalance,
+    contractBalance,
     isBalanceLoading,
     isBalanceError,
     deposit,
+    grabRedPacket,
   } = UseRedPacket();
-  const [amount, setAmount] = useState<string|number>('');
+  const [amount, setAmount] = useState<string | number>('');
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <ConnectButton />
-
+        <ConnectButton showBalance={false}/>
+        {accountBalance && <div>Account Balance: {accountBalance.formatted}{accountBalance.symbol}</div>}
         <div>
-          {isBalanceLoading && <p>Loading balance...</p>}
-          {isBalanceError && <p>Error loading balance</p>}
-          {balance ? <p>Contract Balance:{ } {(Number(balance) / 1e18)} ETH</p> : <p>0 ETH</p>}
+          {isBalanceLoading && <p>Loading contractBalance...</p>}
+          {isBalanceError && <p>Error loading contractBalance</p>}
+          {contractBalance ? <p>Contract Balance:{(Number(contractBalance) / 1e18)} ETH</p> : <p>0 ETH</p>}
         </div>
-        <div>
-          <h1 className="text-3xl font-bold underline">
-            Hello world!
-          </h1>
+        <div className="flex justify-start items-center gap-4">
           <Input value={amount} onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
             setAmount(Number(e.target.value));
           }} type="text" />
           <Button onClick={() => { deposit(BigInt(amount)) }}>充值</Button>
+        </div>
+        <div className="flex justify-start items-center gap-4">
+          <Button onClick={() => { grabRedPacket() }}>抢红包</Button>
         </div>
       </main>
     </div>
--- lib/hooks/useRedPacket.ts
+++ lib/hooks/useRedPacket.ts
@@ -1,40 +1,81 @@
+import { parseEther } from 'viem'
 import { AssignRedPacketAddress, AssignRedPacketAbi } from "@/lib/abi/AssignRedPacket";
-import { useAccount,useWatchBlockNumber, useReadContract, useWriteContract } from "wagmi";
-
+import { useAccount, useBalance, useWatchBlockNumber, useReadContract, useWriteContract,useWaitForTransactionReceipt } from "wagmi";
+type HashType = `0x${string}`;
+// 错误信息解析函数
+function parseRevertReason(err: any): string {
+    if (typeof err?.shortMessage === 'string') return err.shortMessage
+    if (typeof err?.message === 'string') {
+        const match = err.message.match(/revert\s([^'"]+)/i)
+        return match?.[1] ?? 'Transaction reverted'
+    }
+    return 'Unknown error'
+}
 export function UseRedPacket() {
-    const { address: account } = useAccount();
-    const { data: balance, refetch: refetchBalance, isLoading: isBalanceLoading, isError: isBalanceError } = useReadContract({
+    const { address: accountAddress } = useAccount();
+    const { data: accountBalance, refetch: refetchAccountBalance } = useBalance({
+        address: accountAddress,
+        unit: 'ether',
+    });
+    const { data: contractBalance, refetch: refetchContractBalance, isLoading: isBalanceLoading, isError: isBalanceError } = useReadContract({
         address: AssignRedPacketAddress,
         abi: AssignRedPacketAbi.abi,
         functionName: 'getBalance',
     })
 
-    const { writeContract } = useWriteContract();
+    const { writeContract,writeContractAsync } = useWriteContract();
+    // 等待交易完成
+    const waitTransaction = async (hash:HashType) => {
+        try {
+            const { isLoading, isSuccess } = useWaitForTransactionReceipt({ hash })
+        } catch (err:any) {
+            console.log('waitTransaction:',err)
+        }
+    }
+    // 充值
     const deposit = async (amount: bigint) => {
         try {
             await writeContract({
                 address: AssignRedPacketAddress,
                 abi: AssignRedPacketAbi.abi,
                 functionName: 'deposit',
-                value: amount * BigInt(1e18), // Amount in wei
+                value: parseEther(amount.toString()),
             });
-            // refetchBalance(); // Refresh balance after deposit
         } catch (error) {
             console.error('Deposit failed:', error);
+        }
+    };
+    // 抢红包
+    const grabRedPacket = async () => {
+        try {
+            const grabHash = await writeContractAsync({
+                address: AssignRedPacketAddress,
+                abi: AssignRedPacketAbi.abi,
+                functionName: 'grabRedPacket',
+            });
+            console.log('grabRedPacket hash:', grabHash);
+            // waitTransaction(grabHash)
+            return grabHash;
+        } catch (err:any) {
+            console.log('grabRedPacket:',err)
+            return null;
         }
     };
 
     useWatchBlockNumber({
         onBlockNumber: (blockNumber) => {
             console.log('New block number:', blockNumber);
-            refetchBalance(); // Optionally refresh balance on new block
+            refetchAccountBalance();
+            refetchContractBalance();
         },
     })
     return {
-        balance,
+        accountBalance,
+        contractBalance,
         isBalanceLoading,
         isBalanceError,
-        refetchBalance,
+        refetchContractBalance,
         deposit,
+        grabRedPacket
     };
 }
